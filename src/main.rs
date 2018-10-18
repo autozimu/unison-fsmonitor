7e425e696e3a79a5199ed7326ef89307f0a30417
Junfeng Li <lijunfen@amazon.com>
2018-10-17 19:50:56 -0700

    Simplify.

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,25 @@ extern crate percent_encoding;
 
 use failure::Error;
 use std::process::exit;
-use std::ops::Deref;
 
 type Result<R> = std::result::Result<R, Error>;
 
 // TODO: handle sigint.
 
-fn encode(s: &str) -> String {
+fn encode(s: &str) -> impl AsRef<str> {
     return percent_encoding::utf8_percent_encode(s, percent_encoding::SIMPLE_ENCODE_SET)
         .to_string();
 }
 
 fn decode<'a>(s: &'a str) -> impl AsRef<str> + 'a {
-    return percent_encoding::percent_decode(s.as_bytes())
-        .decode_utf8_lossy()
-        .deref();
+    return percent_encoding::percent_decode(s.as_bytes()).decode_utf8_lossy();
 }
 
 fn send_cmd(cmd: &str, args: &[&str]) {
     let mut output = cmd.to_owned();
     for arg in args {
         output += " ";
-        output += &encode(arg);
+        output += &encode(arg).as_ref();
     }
     println!("{}", output);
 }
@@ -39,13 +36,13 @@ fn error(msg: &str) {
     exit(1);
 }
 
-fn recv_cmd<'a>() -> Vec<&'a str> {
+fn recv_cmd() -> Vec<String> {
     // TODO: Handle EOF
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let mut cmd = vec![];
     for word in input.split_whitespace() {
-        cmd.push(decode(word).as_ref())
+        cmd.push(decode(word).as_ref().to_owned())
     }
     cmd
 }
@@ -56,18 +53,20 @@ fn main() -> Result<()> {
     send_cmd("VERSION", &["1"]);
 
     let input = recv_cmd();
-    if input.get(0) != Some(&"VERSION".into()) {
-        bail!("unexpected version cmd: {:?}", input.get(0));
-    }
-    if input.get(1) != Some(&"1".into()) {
-        bail!("unexpected version: {:?}", input.get(1));
-    }
-
-    let empty_string = "";
+    match input
+        .iter()
+        .map(String::as_str)
+        .collect::<Vec<&str>>()
+        .as_slice()
+    {
+        ["VERSION", "1"] => (),
+        ["VERSION", _] => bail!("unexpected version: {:?}", input.get(1)),
+        _ => bail!("unexpected version cmd: {:?}", input.get(0)),
+    };
 
     loop {
         let input = recv_cmd();
-        let cmd = input.get(0).map(String::as_str).unwrap_or(&empty_string);
+        let cmd = input.get(0).cloned().unwrap_or_default();
 
         if cmd == "DEBUG" {
         } else if cmd == "START" {
