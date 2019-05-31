83055cd56025d6628e30394bc8c4a638777786ed
Junfeng Li <lijunfen@amazon.com>
2019-05-31 16:56:33 -0700

    Fix changed paths reporting when path (subdir) is specified.
    See #3 for details.

--- src/main.rs
+++ src/main.rs
@@ -91,12 +91,22 @@ type Id = String;
 
 #[derive(Debug, Default)]
 struct Replica {
+    pub root: PathBuf,
+    /// Currently being watched paths.
     pub paths: HashSet<PathBuf>,
     pub pending_changes: HashSet<PathBuf>,
 }
 
 impl Replica {
-    /// Check if path is contained in this replica.
+    pub fn new(root: PathBuf) -> Replica {
+        Replica {
+            root,
+            paths: HashSet::new(),
+            pending_changes: HashSet::new(),
+        }
+    }
+
+    /// Check if path is being watched in this replica.
     pub fn contains_path(&self, path: &Path) -> bool {
         self.paths.iter().any(|base| path.starts_with(base))
     }
@@ -149,20 +159,22 @@ impl<WATCH: Watch, WRITE: Write> Monitor<WATCH, WRITE> {
                         // START 123 root
                         // START 123 root subdir
                         let replica_id = args[0].clone();
-                        self.current_path = PathBuf::from(&args[1]);
+                        let root = PathBuf::from(&args[1]);
+                        self.current_path = root.clone();
 
                         if let Some(dir) = args.get(2) {
                             self.current_path = self.current_path.join(dir);
                         }
 
-                        if !self.contains_path(&self.current_path) {
+                        let replica = self
+                            .replicas
+                            .entry(replica_id)
+                            .or_insert_with(|| Replica::new(root));
+
+                        if !replica.contains_path(&self.current_path) {
                             self.watcher
                                 .watch(&self.current_path, RecursiveMode::Recursive)?;
-                            self.replicas
-                                .entry(replica_id)
-                                .or_default()
-                                .paths
-                                .insert(self.current_path.clone());
+                            replica.paths.insert(self.current_path.clone());
                         }
 
                         debug!("replicas: {:?}", self.replicas);
@@ -237,11 +249,9 @@ impl<WATCH: Watch, WRITE: Write> Monitor<WATCH, WRITE> {
 
                     for (id, replica) in self.replicas.iter_mut() {
                         for path in &paths {
-                            for dir in &replica.paths {
-                                if let Ok(relative_path) = path.strip_prefix(dir) {
-                                    matched_replica_ids.insert(id);
-                                    replica.pending_changes.insert(relative_path.into());
-                                }
+                            if let Ok(relative_path) = path.strip_prefix(&replica.root) {
+                                matched_replica_ids.insert(id);
+                                replica.pending_changes.insert(relative_path.into());
                             }
                         }
                     }
